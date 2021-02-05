93178370ff68f2ca6b346826b99ea8edd7a480e3
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-05 10:55:36 +0700

    Create en_US.ts

---
+++ src/locales/en_US.ts
@@ -0,0 +1,7 @@
+export default {
+    //Music
+    ADDED_TO_QUEUE: 'Added {MUSIC} to queue',
+    STARTED_PLAYING_MUSIC: 'Start Playing',
+    MUSIC_PROMPT: 'What music you want to play?',
+    MUSIC_PROMT_2: 'you have {TIME} to decide.'
+}
