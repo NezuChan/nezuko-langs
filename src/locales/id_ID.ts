bb455d26d330925bbe42f07edab79a89ed33aeff
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-05 11:07:16 +0700

    Update id_ID.ts

--- src/locales/id_ID.ts
+++ src/locales/id_ID.ts
@@ -2,8 +2,8 @@ export default {
     //Music
     ADDED_TO_QUEUE: 'Menambahkan {MUSIC} ke dalam antrian music',
     STARTED_PLAYING_MUSIC: 'Mulai Memainkan',
-    MUSIC_SKIPPED: 'Melewati music',
-    MUSIC_STOPPED: 'Menghentikan musi',
+    MUSIC_SKIPPED: 'Melewati musik',
+    MUSIC_STOPPED: 'Menghentikan musik',
     MUSIC_PAUSED: 'Memberhentikan musik',
     MUSIC_RESUMED: 'Melanjutkan musik',
     MUSIC_PROMPT: 'Music Apa yang mau kamu mainkan?',
