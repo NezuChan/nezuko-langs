e61570fd2ff1dd264e0d9143601e3d6df2c6ae7f
Devstore120 <78604345+Devstore120@users.noreply.github.com>
2021-02-05 22:03:30 +0700

    Fixed Japanese text formatting.

--- src/locales/ja_JP.ts
+++ src/locales/ja_JP.ts
@@ -6,11 +6,11 @@ export default {
     MUSIC_STOPPED: '停止した音楽',
     MUSIC_PAUSED: '一時停止した音楽',
     MUSIC_RESUMED: '再開された音楽',
-    MUSIC_END: '曲が足りなくなりました！ さらにいくつかの曲をキューに入れてください。',
-    MUSIC_END_2: '歌がなくなった',
+    MUSIC_END: '私たちは曲を使い果たしました!より良いいくつかのより多くの曲をキューに入れます。',
+    MUSIC_END_2: 'ラン・アウト・オブ・ソング',
     MUSIC_IN_VOICE: 'これを使用するには、音声チャネルにいる必要があります',
     MUSIC_HAVE_PERM: '私はあなたの音声チャンネルで許可を持っていません',
-    MUSIC_NO_RESULTS: '',
-    MUSIC_PROMPT: '指定された引数で結果が見つかりません',
-    MUSIC_PROMPT_2: '決めるのは {TIME} です。'
+    MUSIC_NO_RESULTS: '引数に結果が見つかりません',
+    MUSIC_PROMPT: 'どの音楽を再生したいですか?',
+    MUSIC_PROMPT_2: 'あなたは決める {TIME} があります。'
 }
