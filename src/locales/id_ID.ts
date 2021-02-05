7fea6481d5420942348663d545e033507618d9f5
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-05 10:55:14 +0700

    Create id_ID.ts

---
+++ src/locales/id_ID.ts
@@ -0,0 +1,11 @@
+export default {
+    //Music
+    ADDED_TO_QUEUE: 'Menambahkan {MUSIC} ke dalam antrian music',
+    STARTED_PLAYING_MUSIC: 'Mulai Memainkan',
+    MUSIC_SKIPPED: 'Melewati music',
+    MUSIC_STOPPED: 'Menghentikan musi',
+    MUSIC_PAUSED: 'Memberhentikan musik',
+    MUSIC_RESUMED: 'Melanjutkan musik',
+    MUSIC_PROMPT: 'Music Apa yang mau kamu mainkan?',
+    MUSIC_PROMT_2: 'Kamu mempunyai {TIME} untuk menentukan.'
+}
