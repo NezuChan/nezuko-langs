523e695a4bc0969fca6f5bd2db3fe847992ea6c3
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-05 20:16:41 +0700

    Update ja_JP.ts

--- src/locales/ja_JP.ts
+++ src/locales/ja_JP.ts
@@ -1,1 +1,16 @@
-
+export default {
+    //Music
+    ADDED_TO_QUEUE: 'キューに {MUSIC} を追加しました',
+    STARTED_PLAYING_MUSIC: '再生を開始します',
+    MUSIC_SKIPPED: 'スキップされた音楽',
+    MUSIC_STOPPED: '停止した音楽',
+    MUSIC_PAUSED: '一時停止した音楽',
+    MUSIC_RESUMED: '再開された音楽',
+    MUSIC_END: '曲が足りなくなりました！ さらにいくつかの曲をキューに入れてください。',
+    MUSIC_END_2: '歌がなくなった',
+    MUSIC_IN_VOICE: 'これを使用するには、音声チャネルにいる必要があります',
+    MUSIC_HAVE_PERM: '私はあなたの音声チャンネルで許可を持っていません',
+    MUSIC_NO_RESULTS: '',
+    MUSIC_PROMPT: '指定された引数で結果が見つかりません',
+    MUSIC_PROMPT_2: '決めるのは {TIME} です。'
+}
