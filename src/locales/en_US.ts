a8c6cd97edfa21ae7488b4f1cb69ca1c3a3477dd
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-05 11:06:59 +0700

    Update en_US.ts

--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -2,6 +2,10 @@ export default {
     //Music
     ADDED_TO_QUEUE: 'Added {MUSIC} to queue',
     STARTED_PLAYING_MUSIC: 'Start Playing',
+    MUSIC_SKIPPED: 'Skipped music',
+    MUSIC_STOPPED: 'Stopped music',
+    MUSIC_PAUSED: 'Paused music',
+    MUSIC_RESUMED: 'Resumed music',
     MUSIC_PROMPT: 'What music you want to play?',
     MUSIC_PROMT_2: 'you have {TIME} to decide.'
 }
