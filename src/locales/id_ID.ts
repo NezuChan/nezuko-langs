22145913d13f94f44d2ae9e86c751fecae296b63
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-06 17:52:04 +0700

    Update id_ID.ts

--- src/locales/id_ID.ts
+++ src/locales/id_ID.ts
@@ -1,5 +1,5 @@
 export default {
-    //Music
+    /**MUSIC MODULES */
     ADDED_TO_QUEUE: 'Menambahkan {MUSIC} ke dalam antrian music',
     STARTED_PLAYING_MUSIC: 'Mulai Memainkan',
     MUSIC_SKIPPED: 'Melewati musik',
@@ -9,8 +9,19 @@ export default {
     MUSIC_END: 'Antrian sudah kosong! Masukkan lebih banyak lagu ke antrian untuk mendengarkan musik kembali.',
     MUSIC_END_2: 'Kehabisan lagu!',
     MUSIC_IN_VOICE: 'Kamu harus berada di voice channel untuk menggunakan ini.',
-    MUSIC_HAVE_PERM: 'Aku tidak memiliki izin untuk join ke voice channel mu',
+    MUSIC_HAVE_PERM_CONNECT: 'Aku tidak memiliki izin untuk join ke *voice channel* mu',
     MUSIC_NO_RESULTS: 'Tidak dapat menemukan hasil dari *argument* yang di berikan.',
     MUSIC_PROMPT: 'Music Apa yang mau kamu mainkan?',
-    MUSIC_PROMPT_2: 'Kamu mempunyai {TIME} untuk menentukan.'
+    MUSIC_PROMPT_2: 'Kamu mempunyai {TIME} untuk menentukan.',
+    MUSIC_NO_QUEUE: 'Tidak ada musik yang bermain.',
+    MUSIC_SAME_VOICE: 'Kamu harus berada di *voice channel* yang sama dengan ku',
+    MUSIC_EXCEED_LIMIT: 'Maaf, antrian music di *server* ini telah mencapai batas yaitu {LIMIT} musik in dalam antrian.',
+    MUSIC_IS_PLAYING: 'Tidak dapat melanjutkan musik yang telah bermain',
+    
+    /**UTIL MODULES */
+    UTIL_COMMAND_HELP: 'Perintah',
+    UTIL_COMMAND_HELP_2: 'Daftar perintah yang tersedia.\nUntuk info perintah lebih lanjut silahkan ketik {PREFIX}',
+    UTIL_COMMAND_HELP_DESCRIPTION: 'Deskripsi',
+    UTIL_COMMAND_HELP_ALIASES: 'Alias',
+    UTIL_COMMAND_HELP_EXAMPLES: 'Contoh'
 }
