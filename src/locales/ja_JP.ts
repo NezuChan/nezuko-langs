e8a6acec1543ab8c844bbaaedcb275b442f75726
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-06 17:55:26 +0700

    Update ja_JP.ts

--- src/locales/ja_JP.ts
+++ src/locales/ja_JP.ts
@@ -1,4 +1,8 @@
+import en_US from './en_US'
 export default {
+    /**DEFAULT VALUE */
+    ...en_US,
+
     //Music
     ADDED_TO_QUEUE: 'キューに {MUSIC} を追加しました',
     STARTED_PLAYING_MUSIC: '再生を開始します',
@@ -9,7 +13,7 @@ export default {
     MUSIC_END: '私たちは曲を使い果たしました!より良いいくつかのより多くの曲をキューに入れます。',
     MUSIC_END_2: 'ラン・アウト・オブ・ソング',
     MUSIC_IN_VOICE: 'これを使用するには、音声チャネルにいる必要があります',
-    MUSIC_HAVE_PERM: '私はあなたの音声チャンネルで許可を持っていません',
+    MUSIC_HAVE_PERM_CONNECT: '私はあなたの音声チャンネルで許可を持っていません',
     MUSIC_NO_RESULTS: '引数に結果が見つかりません',
     MUSIC_PROMPT: 'どの音楽を再生したいですか?',
     MUSIC_PROMPT_2: 'あなたは決める {TIME} があります。'
