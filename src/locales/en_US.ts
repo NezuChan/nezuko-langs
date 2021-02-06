3b80a5d2ecbc1abe6cc408eab06efd138d52b382
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-06 17:52:21 +0700

    Update en_US.ts

--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -1,16 +1,27 @@
 export default {
-    //Music
+    /**MUSIC MODULES */
     ADDED_TO_QUEUE: 'Added {MUSIC} to queue',
     STARTED_PLAYING_MUSIC: 'Start Playing',
     MUSIC_SKIPPED: 'Skipped music',
     MUSIC_STOPPED: 'Stopped music',
     MUSIC_PAUSED: 'Paused music',
     MUSIC_RESUMED: 'Resumed music',
-    MUSIC_END: 'We\'\ve run out of songs! Better queue up some more tunes.',
+    MUSIC_END: 'We\'ve run out of songs! Better queue up some more tunes.',
     MUSIC_END_2: 'Ran Out Of Song',
     MUSIC_IN_VOICE: 'You must be in voice channel to use this',
-    MUSIC_HAVE_PERM: 'I dont have permission in your voice channel',
+    MUSIC_HAVE_PERM_CONNECT: 'I dont have permission in your voice channel',
     MUSIC_NO_RESULTS: 'No Result found with given argument',
     MUSIC_PROMPT: 'What music you want to play?',
-    MUSIC_PROMPT_2: 'you have {TIME} to decide.'
+    MUSIC_PROMPT_2: 'you have {TIME} to decide.',
+    MUSIC_NO_QUEUE: 'There are no music playing.',
+    MUSIC_SAME_VOICE: 'You must be same voice channel as me',
+    MUSIC_EXCEED_LIMIT: 'Sorry the queue has exceeded the limit {LIMIT} music in queue',
+    MUSIC_IS_PLAYING: 'Cant resume that music already playing',
+
+    /**UTIL MODULES */
+    UTIL_COMMAND_HELP: 'Commands',
+    UTIL_COMMAND_HELP_2: 'A list of available commands.\nFor additional info on a command, type {PREFIX}',
+    UTIL_COMMAND_HELP_DESCRIPTION: 'Description',
+    UTIL_COMMAND_HELP_ALIASES: 'Aliases',
+    UTIL_COMMAND_HELP_EXAMPLES: 'Examples'
 }
